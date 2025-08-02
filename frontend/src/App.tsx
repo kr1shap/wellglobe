b774970d5c2e5d4eaed6b2599664f6e15fa1c22a
Krisha <krisha9845@gmail.com>
2025-08-02 11:18:17 -0400

    ui sample change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import VaxCard from './components/vaxCard';
 
 function App() {
 
@@ -5,8 +6,8 @@ function App() {
     <div className="flex flex-col justify-center items-center">
       <p className="text-2xl text-amber-500">yo</p>
       <div className="bg-amber-700 w-3xl">hello this is a sample usage of tailwind</div>
-      <p className="text-xs text-blue-800 md:bg-amber-400 bg-amber-800">as you can see, it is much easier instead of having seperate css file</p>
       <p>what md: means is, if screen is greater or equal to 'medium' size, then use bg-amber-400 colour. Else use 800</p>
+      <VaxCard/>
     </div>
   )
 }
--- frontend/src/components/vaxCard.tsx
+++ frontend/src/components/vaxCard.tsx
@@ -1,9 +1,27 @@
+type props = {
+  vaxName?: String;
+  description?: String;
+};
 
-function vaxCard() { 
-    return(<>
-    
-    </>
-    )
+function VaxCard(prop: props) {
+  return (
+    <div className="bg-blue-200/80 rounded-3xl border-2 border-blue-950 p-3 flex flex-row items-center max-w-[350px] h-[80px] w-full overflow-hidden">
+      {/* image div */}
+      <div className="mr-2">
+        <i className="bi bi-capsule"></i>
+      </div>
+
+      {/* description div */}
+      <div className="overflow-hidden">
+        <p className="text-[15px] font-bold transition-colors duration-300 hover:text-blue-700 truncate">
+          {prop.vaxName ?? "No vax name"}
+        </p>
+        <p className="text-xs font-medium transition-colors duration-300 hover:text-blue-900 line-clamp-2">
+          {prop.description ?? "No description"}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default vaxCard
+export default VaxCard;
